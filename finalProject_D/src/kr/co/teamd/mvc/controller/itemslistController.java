3c51c6932215f945c58baef85af4f17ef6812295
Canon <Canon@DESKTOP-PL2F7PK>
2020-02-11 18:22:07 +0900

    낚시 디테일 3 - 컨트롤러에 메서드 추가

--- finalProject_D/src/kr/co/teamd/mvc/controller/itemslistController.java
+++ finalProject_D/src/kr/co/teamd/mvc/controller/itemslistController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.teamd.mvc.dao.hostinterdao;
+import kr.co.teamd.mvc.dto.HostDTO;
+import kr.co.teamd.mvc.dto.HostgoodsDTO;
 import kr.co.teamd.mvc.dto.HostlistDTO;
 
 @Controller
@@ -29,13 +32,39 @@ public class itemslistController {
 		return mav;
 	}
 	
+	@GetMapping(value = "itemdetail")
+	public ModelAndView hostDetail(int hnum) {
+		ModelAndView mav = new ModelAndView("itemdetail");
+		HostDTO hdto = hostdao.hostDetail(hnum);
+		List<HostgoodsDTO> hgdtoList = hdto.getHgdto();
+		String[] hspeciesList = hdto.getHspecies().trim().split(",");
+		String[] hserviceList = hdto.getHservice().trim().split(",");
+		mav.addObject("host", hdto);
+		mav.addObject("hglist", hgdtoList);
+		mav.addObject("hspeciesList", hspeciesList);
+		mav.addObject("hserviceList", hserviceList);
+		return mav;
+	}
 	
 	
 	
 	
 	
 	
-
-
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
 }
