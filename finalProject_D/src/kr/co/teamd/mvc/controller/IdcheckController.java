419f34a1a8db3e4bc54c0bfced38325188c6fc95
Canon <Canon@DESKTOP-PL2F7PK>
2020-02-12 10:18:14 +0900

    재민

--- finalProject_D/src/kr/co/teamd/mvc/controller/IdcheckController.java
+++ finalProject_D/src/kr/co/teamd/mvc/controller/IdcheckController.java
@@ -1,14 +1,12 @@
 package kr.co.teamd.mvc.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IdcheckController {
 	
-	@Autowired
-	
+
 	@RequestMapping(value ="/idcheck" )
 	public String idchk(String id) {
 		System.out.println("Id :" + id);
