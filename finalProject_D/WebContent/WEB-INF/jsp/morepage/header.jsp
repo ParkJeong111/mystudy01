43cf6ec7da392b26c84ea6624b50d2f85a766fc7
Jang Sungil <jasuil@daum.net>
2020-02-25 14:56:00 +0900

    주성중 / 함께자바 기능 추가

--- finalProject_D/WebContent/WEB-INF/jsp/morepage/header.jsp
+++ finalProject_D/WebContent/WEB-INF/jsp/morepage/header.jsp
@@ -1271,4 +1271,5 @@ label {
  		
 });
 </script>
+
     <!-- Header End -->
--- finalProject_D/src/kr/co/teamd/mvc/controller/JsonController.java
+++ finalProject_D/src/kr/co/teamd/mvc/controller/JsonController.java
@@ -1,5 +1,6 @@
 package kr.co.teamd.mvc.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -189,7 +190,14 @@ public class JsonController {
 	@RequestMapping(value = "matchingb")
 	public List<MatchingboardDTO> matchingb(MatchingboardDTO dto){
 		System.out.println(dto.getMbtag());
-		String[] service = dto.getMbtag().split("/");
+		List<String> service = new ArrayList<String>();
+		String[] test = dto.getMbtag().split("/");
+		for(int i=0;i<test.length;i++) {
+			service.add(test[i]);
+		}
+		for(String e : service) {
+			System.out.println("e:"+e);
+		}
 		HashMap<String, Object> list = new HashMap<String, Object>();
 		String md = "0";
 		String[] modifysdate = dto.getStartdate().split("/");
@@ -213,7 +221,9 @@ public class JsonController {
 		list.put("list", service);
 		dto.setEnddate(modifyedate[2].substring(2, 4) + "/" + modifyedate[0] + "/" + modifyedate[1]);
 		List<MatchingboardDTO> searchdto = MatchingBoard.optionsearch(list);
-		
+		for(MatchingboardDTO e : searchdto) {
+			System.out.println(e.getMbtitle());
+		}
 		return searchdto;
 	}
 	
